87ef1574cd26babc2e4461e19c20269d45a8c246
jdlehman <jonathan.lehman91@gmail.com>
2014-02-08 06:22:47 -0500

    Add ngRoute, and add routes.

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,1 +1,18 @@
-var staticApp = angular.module('staticApp', []);
+var staticApp = angular.module('staticApp', ['ngRoute', 'firebase'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html'
+      })
+      .when('/brainstorm/:id', {
+        templateUrl: 'views/brainstorm.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+  }]);
+
